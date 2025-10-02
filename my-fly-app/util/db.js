50fcdc833e1288d9684dd827b422f44025171201
Cnnb01 <nyamambanyaguch@gmail.com>
2025-10-02 16:48:30 +0300

    Adding migrations

--- my-fly-app/util/db.js
+++ my-fly-app/util/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const { DATABASE_URL } = require("./config");
+const { Umzug, SequelizeStorage } = require("umzug");
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -14,10 +15,26 @@ const sequelize = new Sequelize(DATABASE_URL, {
       }
     : {},
 });
+// const sequelize = new Sequelize(DATABASE_URL);
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: "migrations/*.js",
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  });
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
 
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     console.log("Connection has been established successfully.");
     // sequelize.close();
   } catch (error) {
