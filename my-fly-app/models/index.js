e7d04447d394583c36b55c70b60f8e009402c77e
Cnnb01 <nyamambanyaguch@gmail.com>
2025-10-06 14:34:28 +0300

    Adding admin user and user disabling migration

--- my-fly-app/models/index.js
+++ my-fly-app/models/index.js
@@ -4,8 +4,8 @@ const User = require("./user");
 User.hasMany(Note);
 Note.belongsTo(User);
 
-// User.sync({ alter: true });
-// Note.sync({ alter: true }); //creates the table note if it doesn't exist
+User.sync({ alter: true });
+Note.sync({ alter: true }); //creates the table note if it doesn't exist
 
 module.exports = {
   Note,
